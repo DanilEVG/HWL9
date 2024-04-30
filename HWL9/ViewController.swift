b5ec05c74dcecd82fd32aacab048115d9fd59480
DanilEVG <89922067067@mail.ru>
2024-04-30 12:13:31 +0500

    Initial Commit

---
+++ HWL9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWL9
+//
+//  Created by Даниил Евгеньевич on 30.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
